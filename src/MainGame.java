e720417c1184ec09c0f45f77fc8a4bd8c10209b2
ollybrock <obrock@macalester.edu>
2025-11-24 11:48:25 -0600

    fleshed out main class
    started adding constructors

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,3 +1,23 @@
+import edu.macalester.graphics.CanvasWindow;
+
 public class MainGame {
-    
+    private final double CANVAS_HEIGHT = 600;
+    private final double CANVAS_WIDTH = 600; 
+    private Slime slime;
+    private Crate crate;
+
+    private CanvasWindow canvas;
+
+    public static void main(String[] args){
+        new MainGame();
+    }
+
+    public MainGame() {
+        CanvasWindow canvas = new CanvasWindow("Slimekoban!", (int)CANVAS_WIDTH, (int)CANVAS_HEIGHT);
+        Slime slime = new Slime();
+        Crate crate = new Crate();
+
+    }
+
+
 }
