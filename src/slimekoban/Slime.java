3c4cd591279acc09c2a00fba020003b7d2a00019
Maren <MGood8086@gmail.com>
2025-12-01 11:18:28 -0600

    fixed

--- src/slimekoban/Slime.java
+++ src/slimekoban/Slime.java
@@ -5,22 +5,16 @@ import edu.macalester.graphics.Point;
 import edu.macalester.graphics.Rectangle;
 import edu.macalester.graphics.GraphicsGroup;
 
+
 public class Slime {
     private Point slimePos;
     private final double SLIME_SIZE = 20;
-    private double width;
-    private double height;
     private Rectangle slimeRectangle;
 
     public Slime(Point slimePos) {
         this.slimePos = slimePos;
-        
+        slimeRectangle = new Rectangle(slimePos.getX(), slimePos.getY(), SLIME_SIZE, SLIME_SIZE);
+        slimeRectangle.setFillColor(Color.WHITE);
     }
+} 
 
-    public void createSlime(GraphicsGroup graphicsGroup) {
-        Rectangle slime = new Rectangle(slimePos.getX(), slimePos.getY(), SLIME_SIZE, SLIME_SIZE);
-        slime.setFillColor(Color.WHITE);
-        graphicsGroup.add(slime);
-    }
-
-}
