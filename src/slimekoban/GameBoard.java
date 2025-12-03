2fcd7f3a347b897640acdb07b8af4e3c13272eb3
ollybrock <obrock@macalester.edu>
2025-12-03 11:21:50 -0600

    added canvas dim. getters
    started working on gameBoard

--- src/slimekoban/GameBoard.java
+++ src/slimekoban/GameBoard.java
@@ -1,5 +1,17 @@
 package slimekoban;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class GameBoard {
-    
+    List<GridCell> cells = new ArrayList<>();
+
 }
+
+    
+
+// is there an existing cell where i want to go?
+    // can i move there?
+        // is there something in the neighbor cell?
+        // is there something in the nc's nc?
